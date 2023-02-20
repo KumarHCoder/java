57c502ee1f8416931b6cd3f5adae7e191e13da39
KumarHCoder <kumarkankam21@gmail.com>
2023-02-20 15:45:07 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int Student_No=1001;
+        int Teacher_No=5001;
+        String Student_Name="Kumar_Shankar_Kankam";
+        String Teacher_Name="Harsha_Urdurkar";
+        System.out.println(Student_Name);
+        System.out.println(Student_No);
+        System.out.println(Teacher_No);
+        System.out.println(Teacher_Name);
+
+    }
+}
